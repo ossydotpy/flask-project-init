0e98b7d428d1c5fd3b611557bad27891b94dd535
osman <99814325+ossydotpy@users.noreply.github.com>
2024-06-30 13:05:26 +0000

    fix install script

--- install.sh
+++ install.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
+
+
+INSTALL_DIR="$HOME/bin"
+
+# URLs for the scripts
 FLASK_INIT_URL="https://raw.githubusercontent.com/ossydotpy/flask-project-init/master/init_flask.sh"
 STARTER_SCRIPTS_URL="https://raw.githubusercontent.com/ossydotpy/flask-project-init/master/starter_scripts.sh"
 
-curl -L $FLASK_INIT_URL -o /usr/local/bin/flask_init.sh
-curl -L $STARTER_SCRIPTS_URL -o /usr/local/bin/starter_scripts.sh
 
-sudo chmod +x /usr/local/bin/flask_init.sh
+install_script() {
+    local url="$1"
+    local script_name="$2"
+    
+    echo "Installing $script_name..."
+    curl -L "$url" -o "$INSTALL_DIR/$script_name"
+    chmod +x "$INSTALL_DIR/$script_name"
+}
+
+
+mkdir -p "$INSTALL_DIR"
+
+
+install_script "$FLASK_INIT_URL" "flask_init"
+install_script "$STARTER_SCRIPTS_URL" "starter_scripts"
+
+echo "Installation complete."
